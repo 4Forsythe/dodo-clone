3c33244c71f0184834eef3710a632e962a734381
Forsythe <forsythe.dev@gmail.com>
2024-09-05 19:51:34 +0400

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Nunito } from 'next/font/google'
+
+import './styles.css'
+
+const nunito = Nunito({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+  variable: '--font-nunito',
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ru">
+      <body className={nunito.variable}>
+        <main className="min-h-screen bg-white">{children}</main>
+      </body>
+    </html>
+  )
+}
