11b9bf63bd738544c814c6db3ce04eee5928d66c
Forsythe <forsythe.dev@gmail.com>
2024-09-07 20:55:34 +0400

    Add constants data for prisma seeding

---
+++ src/constants/seed.constants.ts
@@ -0,0 +1,261 @@
+export const categories = [
+  {
+    name: 'Пиццы',
+  },
+  {
+    name: 'Завтрак',
+  },
+  {
+    name: 'Закуски',
+  },
+  {
+    name: 'Коктейли',
+  },
+  {
+    name: 'Напитки',
+  },
+]
+
+export const ingredients = [
+  {
+    name: 'Сырный бортик',
+    price: 179,
+    imageUrl:
+      'https://cdn.dodostatic.net/static/Img/Ingredients/99f5cb91225b4875bd06a26d2e842106.png',
+  },
+  {
+    name: 'Сливочная моцарелла',
+    price: 79,
+    imageUrl:
+      'https://cdn.dodostatic.net/static/Img/Ingredients/cdea869ef287426386ed634e6099a5ba.png',
+  },
+  {
+    name: 'Сыры чеддер и пармезан',
+    price: 79,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA69C1FE796',
+  },
+  {
+    name: 'Острый перец халапеньо',
+    price: 59,
+    imageUrl:
+      'https://cdn.dodostatic.net/static/Img/Ingredients/11ee95b6bfdf98fb88a113db92d7b3df.png',
+  },
+  {
+    name: 'Нежный цыпленок',
+    price: 79,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA5B328D35A',
+  },
+  {
+    name: 'Шампиньоны',
+    price: 59,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA67259A324',
+  },
+  {
+    name: 'Ветчина',
+    price: 79,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA61B9A8D61',
+  },
+  {
+    name: 'Пикантная пепперони',
+    price: 79,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA6258199C3',
+  },
+  {
+    name: 'Острая чоризо',
+    price: 79,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA62D5D6027',
+  },
+  {
+    name: 'Маринованные огурчики',
+    price: 59,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9EA89958D782B',
+  },
+  {
+    name: 'Свежие томаты',
+    price: 59,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA7AC1A1D67',
+  },
+  {
+    name: 'Красный лук',
+    price: 59,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA60AE6464C',
+  },
+  {
+    name: 'Сочные ананасы',
+    price: 59,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9AFA6795BA2A0',
+  },
+  {
+    name: 'Итальянские травы',
+    price: 39,
+    imageUrl:
+      'https://cdn.dodostatic.net/static/Img/Ingredients/370dac9ed21e4bffaf9bc2618d258734.png',
+  },
+  {
+    name: 'Сладкий перец',
+    price: 59,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA63F774C1B',
+  },
+  {
+    name: 'Кубики брынзы',
+    price: 79,
+    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA6B0FFC349',
+  },
+  {
+    name: 'Митболы',
+    price: 79,
+    imageUrl:
+      'https://cdn.dodostatic.net/static/Img/Ingredients/b2f3a5d5afe44516a93cfc0d2ee60088.png',
+  },
+]
+
+export const products = [
+  {
+    name: 'Сырная 🌱👶',
+    description: 'Моцарелла, сыры чеддер и пармезан, фирменный соус альфредо',
+    categoryId: 1,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.webp',
+  },
+  {
+    name: 'Ветчина и сыр',
+    description: 'Ветчина, моцарелла, фирменный соус альфредо',
+    categoryId: 1,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D60FDA22358AC33C6A44EB093A2.webp',
+  },
+  {
+    name: 'Карбонара',
+    description:
+      'Бекон, сыры чеддер и пармезан, моцарелла, томаты, красный лук, чеснок, фирменный соус альфредо, итальянские травы',
+    categoryId: 1,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61389AB51A8F648A0DBA5B1689.webp',
+  },
+  {
+    name: 'Двойной цыпленок 👶',
+    description: 'Цыпленок, моцарелла, фирменный соус альфредо',
+    categoryId: 1,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D614CBE0530B7234B6D7A6E5F8E.webp',
+  },
+  {
+    name: 'Пепперони',
+    description: 'Пикантная пепперони, увеличенная порция моцареллы, фирменный томатный соус',
+    categoryId: 1,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D610A62D78598406363A9A8AD65.webp',
+  },
+  {
+    name: 'Маргарита 🌱',
+    description: 'Увеличенная порция моцареллы, томаты, итальянские травы, фирменный томатный соус',
+    categoryId: 1,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.webp',
+  },
+  {
+    name: 'Омлет с ветчиной и грибами',
+    description: 'Горячий сытный омлет с поджаристой корочкой, ветчина, шампиньоны и моцарелла',
+    categoryId: 2,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7970321044479C1D1085457A36EB.webp',
+  },
+  {
+    name: 'Омлет с пепперони',
+    description:
+      'Сытный и сбалансированный завтрак — омлет с поджаристой корочкой, пикантная пепперони, томаты и моцарелла',
+    categoryId: 2,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE94ECF33B0C46BA410DEC1B1DD6F8.webp',
+  },
+  {
+    name: 'Кофе Латте',
+    description: 'Когда хочется нежную молочную пенку, на помощь приходит классический латте',
+    categoryId: 2,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+  },
+  {
+    name: 'Дэнвич ветчина и сыр',
+    description:
+      'Поджаристая чиабатта и знакомое сочетание ветчины, цыпленка, моцареллы со свежими томатами, соусом ранч и чесноком',
+    categoryId: 3,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FF0059B799A17F57A9E64C725.webp',
+  },
+  {
+    name: 'Куриные наггетсы',
+    description: 'Нежное куриное мясо в хрустящей панировке',
+    categoryId: 3,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D618B5C7EC29350069AE9532C6E.webp',
+  },
+  {
+    name: 'Картофель из печи с соусом 🌱',
+    description: 'Запеченная в печи картошечка с пряными специями. В комплекте сырный соус',
+    categoryId: 3,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EED646A9CD324C962C6BEA78124F19.webp',
+  },
+  {
+    name: 'Додстер',
+    description:
+      'Легендарная горячая закуска с цыпленком, томатами, моцареллой, соусом ранч в тонкой пшеничной лепешке',
+    categoryId: 3,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796F96D11392A2F6DD73599921B9.webp',
+  },
+  {
+    name: 'Острый Додстер 🌶️🌶️',
+    description:
+      'Горячая закуска с цыпленком, перчиком халапеньо, маринованными огурчиками, томатами, моцареллой и соусом барбекю в тонкой пшеничной лепешке',
+    categoryId: 3,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FD3B594068F7A752DF8161D04.webp',
+  },
+  {
+    name: 'Молочный коктейль Пина Колада',
+    description: 'Тропическое сочетание кокоса и ананаса в нежном милкшейке',
+    categoryId: 4,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EEE20B8772A72A9B60CFB20012C185.webp',
+  },
+  {
+    name: 'Шоколадный молочный коктейль',
+    description:
+      'Очаровательная шоколадная нежность. Попробуйте молочный коктейль с какао и мороженым',
+    categoryId: 4,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE79702E2A22E693D96133906FB1B8.webp',
+  },
+  {
+    name: 'Молочный коктейль с печеньем Орео',
+    description:
+      'Как вкуснее есть печенье? Его лучше пить! Попробуйте молочный коктейль с мороженым и дробленым печеньем «Орео»',
+    categoryId: 4,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FA1F50F8F8111A399E4C1A1E3.webp',
+  },
+  {
+    name: 'Классический молочный коктейль 👶',
+    description:
+      'В мире так много коктейлей, но классика — вечна. Попробуйте наш молочный напиток с мороженым',
+    categoryId: 4,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796F93FB126693F96CB1D3E403FB.webp',
+  },
+  {
+    name: 'Ирландский Капучино',
+    description:
+      'Освежающий напиток для любителей кофе. В составе эспрессо, пломбир, молоко и бодрость на весь день',
+    categoryId: 5,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61999EBDA59C10E216430A6093.webp',
+  },
+  {
+    name: 'Кофе Карамельный капучино',
+    description: 'Если не шоколад, то карамель! А капучино с карамельным сиропом особенно хорош',
+    categoryId: 5,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61AED6B6D4BFDAD4E58D76CF56.webp',
+  },
+  {
+    name: 'Кофе Кокосовый латте',
+    description:
+      'Горячий напиток на основе эспрессо с увеличенной порцией молока и кокосовым сиропом',
+    categoryId: 5,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B19FA07090EE88B0ED347F42.webp',
+  },
+  {
+    name: 'Кофе Американо',
+    description: 'Пара глотков горячего Американо, и вы будете готовы покорять этот день',
+    categoryId: 5,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B044583596548A59078BBD33.webp',
+  },
+  {
+    name: 'Кофе Латте',
+    description: 'Когда хочется нежную молочную пенку, на помощь приходит классический латте',
+    categoryId: 5,
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+  },
+]
