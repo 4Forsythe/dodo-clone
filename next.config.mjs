0a011a73a52120b2fb90b5896a4f0f1c6d6f18d9
Forsythe <forsythe.dev@gmail.com>
2024-09-08 11:29:23 +0400

    Fix: update next.config.mjs to allow external domains for Image

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+  // Загрузка внешних изображений
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+}
 
 export default nextConfig
