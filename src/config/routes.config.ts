48d354982b47f41d13f1e83a268904981a94000f
Forsythe <forsythe.dev@gmail.com>
2024-10-07 23:51:52 +0400

    Upd: add profile | thanks | unauthorized | activate paths in routes.config.ts

--- src/config/routes.config.ts
+++ src/config/routes.config.ts
@@ -2,8 +2,12 @@ class ROUTES {
   private BASE_URL = String(process.env.NEXT_PUBLIC_BASE_URL)
 
   HOME = this.BASE_URL
+  PROFILE = `${this.BASE_URL}/profile`
   PRODUCT = `${this.BASE_URL}/product`
   CHECKOUT = `${this.BASE_URL}/checkout`
+  THANKS = `${this.BASE_URL}/thanks`
+  UNAUTHORIZED = `${this.BASE_URL}/unauthorized`
+  ACTIVATE = `${this.BASE_URL}/api/auth/activate`
 }
 
 export const route = new ROUTES()
