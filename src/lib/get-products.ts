b63691a1ee0ef1dcb759cbbd3476e574ffe2c234
Forsythe <forsythe.dev@gmail.com>
2024-10-19 23:34:03 +0400

    Upd: make getProducts() function argument params is optional

--- src/lib/get-products.ts
+++ src/lib/get-products.ts
@@ -4,17 +4,17 @@ import { DEFAULT_PRICE_FROM, DEFAULT_PRICE_TO } from '@/constants'
 
 import { type IProductParams, ProductSortBy } from '@/types/product.types'
 
-export const getProducts = async (params: IProductParams) => {
+export const getProducts = async (params?: IProductParams) => {
   // Конвертация строковых значений в массивы чисел
-  const sizes = params.sizes?.split(',').map(Number)
-  const types = params.types?.split(',').map(Number)
-  const ingredients = params.ingredients?.split(',').map(Number)
+  const sizes = params?.sizes?.split(',').map(Number)
+  const types = params?.types?.split(',').map(Number)
+  const ingredients = params?.ingredients?.split(',').map(Number)
 
-  const sortBy = params.sortBy
+  const sortBy = params?.sortBy
 
   // Значения разброса по стоимости
-  const from = Number(params.from) || DEFAULT_PRICE_FROM
-  const to = Number(params.to) || DEFAULT_PRICE_TO
+  const from = Number(params?.from) || DEFAULT_PRICE_FROM
+  const to = Number(params?.to) || DEFAULT_PRICE_TO
 
   return prisma.category.findMany({
     include: {
