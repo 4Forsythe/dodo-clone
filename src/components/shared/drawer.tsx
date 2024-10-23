9b93a5d65a134330d4bfe7a451def952645e231d
Forsythe <forsythe.dev@gmail.com>
2024-10-23 21:01:42 +0400

    Fix: update sheet-close button to repair html hydration error

--- src/components/shared/drawer.tsx
+++ src/components/shared/drawer.tsx
@@ -129,9 +129,9 @@ export const Drawer: React.FC<React.PropsWithChildren<IDrawer>> = ({ children, c
             </div>
 
             <SheetClose>
-              <Button className="w-full rounded-2xl" size="lg">
+              <span className="h-10 px-5 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-2xl text-sm font-medium ring-offset-background transition-colors bg-primary text-primary-foreground hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
                 Всё понятно
-              </Button>
+              </span>
             </SheetClose>
           </div>
         )}
